a2246690ef5a31d538d7d751ab0351593d2e9da6
jisoooit <77223718+jisoooit@users.noreply.github.com>
2022-10-24 14:45:53 +0900

    [level 2] Title: 전화번호 목록, Time: 95.30 ms, Memory: 28 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.py
@@ -6,11 +6,6 @@ def solution(phone_book):
     for i in range(l-1):
         if len(phone_book[i]) < len(phone_book[i+1]) and phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
             return False
-    
-#     for i in range(l):
-#         for j in range(i+1,l):
-#             if len(phone_book[i]) < len(phone_book[j]) and phone_book[i] == phone_book[j][:len(phone_book[i])]:
-#                     return False
         
     
     return answer
