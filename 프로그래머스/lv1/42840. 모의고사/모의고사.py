94ab315f2c2803e250ce0e18443d0561da8f1c63
jisoooit <77223718+jisoooit@users.noreply.github.com>
2022-12-04 15:43:38 +0900

    [level 1] Title: 모의고사, Time: 3.47 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.py
@@ -0,0 +1,20 @@
+def solution(answers):
+    answer=[]
+    p1=[1,2,3,4,5]
+    p2=[2,1,2,3,2,4,2,5]
+    p3=[3,3,1,1,2,2,4,4,5,5]
+    n=[0,0,0]
+   
+    for i in range(len(answers)):
+        if p1[i%len(p1)] == answers[i]:
+            n[0]+=1
+        if p2[i%len(p2)] == answers[i]:
+            n[1]+=1
+        if p3[i%len(p3)] == answers[i]:
+            n[2]+=1
+            
+    for i, num in enumerate(n):
+        if max(n) == num:
+            answer.append(i+1)
+    
+    return answer
