9b90dd9c4556f2e92e0d69494f5df2d8f428c055
jisoooit <77223718+jisoooit@users.noreply.github.com>
2022-12-08 13:34:33 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -10,6 +10,15 @@ def dfs(num,visited,computers):
             visited[idx]=1
             dfs(idx,visited,computers)
 
+def bfs(num, visited, computers):
+    q=deque([num])
+    visited[num]=1
+    while q:
+        num = q.popleft()
+        for idx, i in enumerate(computers[num]):
+            if i==1 and visited[idx]==0:
+                visited[idx]=1
+                q.append(idx)
 
 def solution(n, computers):
     answer = 0
@@ -17,7 +26,8 @@ def solution(n, computers):
     
     for i in range(n):
         if visited[i]==0:
-            dfs(i,visited, computers)
+            # dfs(i,visited, computers)
+            bfs(i, visited, computers)
             answer+=1
     
     return answer
