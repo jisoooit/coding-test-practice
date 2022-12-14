d952224d979b2b1370862b965b591546b0873c24
jisoooit <77223718+jisoooit@users.noreply.github.com>
2022-12-14 14:33:12 +0900

    [level 2] Title: 모음 사전, Time: 4.81 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,16 +1,37 @@
 from itertools import product
 
+all=['A','E','I','O','U']
+d=[]
+N=0
+
+def dfs(depth,md):
+    if depth > N:
+        return
+    else:
+        d.append(md)
+    for i in range(len(all)):
+        if len(md+all[i]) <=N: # 이거 없어도 위에 if에서 걸러지지만 아예 dfs 실행 안되게 하고 싶었음..
+            dfs(depth+1, md+all[i])
+
+            
 def solution(word):
     answer=0
+    global N,d
+    N=5
+    dfs(0,"")
+    d=sorted(d)
+    return d.index(word)
+# def solution(word):
+#     answer=0
     
-    all = []
-    dataset = ['A','E','I','O','U']
-    for i in range(1,6):
-        all += list(product(dataset, repeat=i))
+#     all = []
+#     dataset = ['A','E','I','O','U']
+#     for i in range(1,6):
+#         all += list(product(dataset, repeat=i))
 
-    for i, a in enumerate(all):
-        all[i]=''.join(a)
+#     for i, a in enumerate(all):
+#         all[i]=''.join(a)
     
-    all=sorted(all)
+#     all=sorted(all)
 
-    return all.index(word)+1
+#     return all.index(word)+1
