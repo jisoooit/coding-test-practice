4cc083b1ca392df710b0c55a94b8184e4b53fa05
jisoooit <77223718+jisoooit@users.noreply.github.com>
2022-12-14 13:50:23 +0900

    [level 2] Title: 피로도, Time: 0.12 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -1,18 +1,38 @@
 from itertools import permutations
 
-def solution(k, dungeons):
-    answer=0
-    all=list(permutations(range(len(dungeons)), len(dungeons)))
-    case_res=[]
-    for case in all:
-        nowk=k
-        for idx, c in enumerate(case):
-            if nowk >= dungeons[c][0]:
-                nowk-=dungeons[c][1]
-                if idx == len(case)-1:
-                      case_res.append(idx+1)
-            else:
-                case_res.append(idx)
-                break
+answer=0
+def dfs(cnt,k,dungeons):
+    global answer
+    if cnt > answer:
+        answer=cnt
+    for i in range(len(dungeons)):
+        if visited[i]==0 and dungeons[i][0]<=k:
+            visited[i]=1
+            dfs(cnt+1, k-dungeons[i][1],dungeons)
+            visited[i]=0
 
-    return max(case_res)
+            
+def solution(k,dungeons):
+    global visited
+    
+    visited=[0]*len(dungeons)
+    
+    dfs(0,k,dungeons)
+    return answer
+
+# def solution(k, dungeons):
+#     answer=0
+#     all=list(permutations(range(len(dungeons)), len(dungeons)))
+#     case_res=[]
+#     for case in all:
+#         nowk=k
+#         for idx, c in enumerate(case):
+#             if nowk >= dungeons[c][0]:
+#                 nowk-=dungeons[c][1]
+#                 if idx == len(case)-1:
+#                       case_res.append(idx+1)
+#             else:
+#                 case_res.append(idx)
+#                 break
+
+#     return max(case_res)
