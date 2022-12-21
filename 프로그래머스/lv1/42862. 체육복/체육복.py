d8a4b61f2e3b0848c36bf7281ed2400c4392e9c4
Jisu Kweon <77223718+jisoooit@users.noreply.github.com>
2022-12-21 16:50:57 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -11,7 +11,7 @@ def solution(n, lost, reserve):
     
     answer=n-len(lost)
     for l in lost: #작은거부터 올라가니까
-        if l-1 in reserve: #작은거 있으면 부터 검사하는거임
+        if l-1 in reserve: #작은거 있으면 부터 검사하는거임(순서 바꾸면 안됨)
             answer+=1
             reserve.remove(l-1)
         elif l+1 in reserve:
