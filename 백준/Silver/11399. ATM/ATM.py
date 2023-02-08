27d6f8c3a26787f51095237a481e93ed034add0f
Jisu Kweon <77223718+jisoooit@users.noreply.github.com>
2023-02-08 21:03:10 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -3,9 +3,9 @@ arr=list(map(int,input().split()))
 
 arr.sort() # 1 2 3 3 4
 ans=0
+part=0
 answer=[]
-# 1 / 1+2 / 1+2+3
 for i in range(len(arr)):
-    ans += arr[i] # 0+1 / 1+2 / 3+3 / 6+3 / 
-    answer.append(ans)
-print(sum(answer))
+    part += arr[i] # 0+1 / 1+2 / 3+3 / 6+3 / 
+    ans+=part
+print(ans)
