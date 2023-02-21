53649796b2c877e5bc620898a6289e161eea50d3
Jisu Kweon <77223718+jisoooit@users.noreply.github.com>
2023-02-21 20:09:13 +0900

    [Silver IV] Title: 수 찾기, Time: 408 ms, Memory: 46732 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,20 +1,25 @@
-def binarySearch(arr, a2):
-    start =0
-    end = len(arr)-1
-    while start <=end:
-        mid=(start+end)//2
-        if arr[mid] == a2:
-            return 1
-        elif arr[mid] > a2:
-            end=mid-1
-        else:
-            start=mid+1
-    return 0
-
 n=int(input())
 arr=list(map(int,input().split()))
 m=int(input())
 arr2=list(map(int,input().split()))
 arr.sort()
-for a2 in arr2:
-    print(binarySearch(arr,a2))
+
+def search(arr, num):
+    left=0
+    right=n-1
+    while(left<=right):
+        mid = (left+right)//2
+        if arr[mid] == num:
+            return True
+        elif arr[mid] > num:
+            right=mid-1
+        else:
+            left=mid+1
+    
+    return False
+
+for i in range(m):
+    if search(arr,arr2[i]):
+        print(1)
+    else:
+        print(0)
