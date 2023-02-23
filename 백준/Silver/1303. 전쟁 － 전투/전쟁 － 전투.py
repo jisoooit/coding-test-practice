b5f03e38056f6982c1f53d9c0659313f91af839d
Jisu Kweon <77223718+jisoooit@users.noreply.github.com>
2023-02-23 21:44:43 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 60 ms, Memory: 33392 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -1,5 +1,5 @@
-
 import math
+
 n,m = map(int,input().split())
 dy = [-1, 0, 1, 0]
 dx = [0, 1, 0, -1]
@@ -13,27 +13,24 @@ for i in range(m):
     graph.append(list(input()))
 
 def dfs(y,x, color):
-    global cnt
-
+    cnt=1
     visited[y][x]=1
     for i in range(4):
         ny = y+dy[i]
         nx = x+dx[i]
         if 0<=ny<m and 0<=nx<n and visited[ny][nx]==0:
             if graph[ny][nx]==color:
-                cnt+=1
-                dfs(ny,nx,color)
-   
+                cnt += dfs(ny,nx,color)
     return cnt
 
 for i in range(m):
     for j in range(n):
         if graph[i][j]=='W' and visited[i][j]==0:
-            cnt=1
             tw+= math.pow(dfs(i,j,'W'),2)
         elif graph[i][j]=='B' and visited[i][j]==0:
-            cnt=1
             tb+= math.pow(dfs(i,j,'B'),2)
 
 
 print(int(tw), int(tb))
+
+     
